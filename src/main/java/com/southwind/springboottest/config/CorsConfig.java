0df6c1f382ddee6c374f446dfb5675061ff20f52
ahead <1677306192@qq.com>
2022-02-26 18:28:21 +0800

    Default Changelist

---
+++ src/main/java/com/southwind/springboottest/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.southwind.springboottest.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    //解决跨域问题，可以直接使用@CrossOrigin注解
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
+
---
+++ src/main/java/com/southwind/springboottest/controller/BookHandler.java
@@ -0,0 +1,65 @@
+package com.southwind.springboottest.controller;
+
+import com.southwind.springboottest.entity.Book;
+import com.southwind.springboottest.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/book",method = RequestMethod.GET)
+@CrossOrigin()
+public class BookHandler {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/findAll/{page}/{size}")
+    public Page<Book> findAll(@PathVariable("page") Integer page, @PathVariable("size") Integer size){
+        return bookRepository.findAll(PageRequest.of(page - 1, size));
+
+    }
+
+    @GetMapping("/getString")
+    public String getString() {
+        return "123abc";
+    }
+
+    @GetMapping("/findAll2")
+    public List<Book> findAll2() {
+        return bookRepository.findAll();
+    }
+
+
+    @PostMapping("/save")
+    public String save(@RequestBody Book book){
+        Book result = bookRepository.save(book);
+        if(result != null){
+            return "success";
+        }else{
+            return "error";
+        }
+    }
+
+    @GetMapping("/findById/{id}")
+    public Book findById(@PathVariable("id") Integer id){
+        return bookRepository.findById(id).get();
+    }
+
+    @PutMapping("/update")
+    public String update(@RequestBody Book book){
+        Book result = bookRepository.save(book);
+        if(result != null){
+            return "success";
+        }else{
+            return "error";
+        }
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public void deleteById(@PathVariable("id") Integer id){
+        bookRepository.deleteById(id);
+    }
+}
